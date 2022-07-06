8c2af07ec906fcf97e10f819e0dc2aa61469c9b0
bonnett93 <bonnettjordan@gmail.com>
2022-07-06 00:44:40 -0500

    Upload requeriments

---
+++ setup_commands/os_requeriments.sh
@@ -0,0 +1,8 @@
+# Adicitional installations for ubuntu os
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential libssl-dev
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+git lfs track "*.psd"
+git add .gitattributes
+uvicorn fast_api.mlapi:app --reload
